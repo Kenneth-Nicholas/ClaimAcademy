caee2c4cb0f8af5f1d6896c2ff26e558e169879c
Kenneth Nicholas <nicholas.kenneth.j@gmail.com>
2019-01-22 20:34:34 -0600

    FirstJavaProgram

--- firstJavaProgram/src/firstJavaProgram/FirstJavaProgram.java
+++ firstJavaProgram/src/firstJavaProgram/FirstJavaProgram.java
@@ -6,6 +6,24 @@ public class FirstJavaProgram {
 		
 		System.out.println("This is my first program in java");
 		
+		int a = 1;
+		
+		int b = 2;
+		
+		int c = 3;
+		
+		for(int i = 0; i < 10; i++) {
+			
+			a += i;
+			b += a;
+			c += b;
+			
+		}
+		
+		System.out.println("int a is now equal to " + a);
+		System.out.println("int b is now equal to " + b);
+		System.out.println("int c is now equal to " + c);
+		
 	} // End of main
 
 } // End of FirstJavaProgram Class
