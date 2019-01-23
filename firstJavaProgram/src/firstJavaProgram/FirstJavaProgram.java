c2c19de46ba1522f104cfe3ac1ba1270d1218443
Kenneth Nicholas <nicholas.kenneth.j@gmail.com>
2019-01-22 20:25:45 -0600

    FirstJavaProgram

---
+++ firstJavaProgram/src/firstJavaProgram/FirstJavaProgram.java
@@ -0,0 +1,11 @@
+package firstJavaProgram;
+
+public class FirstJavaProgram {
+
+	public static void main(String[] args) {
+		
+		System.out.println("This is my first program in java");
+		
+	} // End of main
+
+} // End of FirstJavaProgram Class
