1eae3c1505da8a7ee6686d75d50e2b3a0312e55b
Kenneth Nicholas <nicholas.kenneth.j@gmail.com>
2019-01-22 18:37:29 -0600

    WelcomeToJava

---
+++ welcomeToJava/src/welcomeToJava/WelcomeToJava.java
@@ -0,0 +1,13 @@
+// This program prints Welcome to Java!
+
+package welcomeToJava;
+
+public class WelcomeToJava {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Welcome to Java!");
+
+	}
+
+}
