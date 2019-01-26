3b684f854de2545c536d32919cf80f5e862b4fdc
Kenneth Nicholas <nicholas.kenneth.j@gmail.com>
2019-01-26 13:02:57 -0600

    ChangeMaker

---
+++ changeMaker/src/changeMaker/ChangeMaker.java
@@ -0,0 +1,83 @@
+package changeMaker;
+
+import java.util.Scanner;
+
+public class ChangeMaker {
+		
+		public static void main(String[] args) {
+			
+			Scanner input = new Scanner(System.in);
+			
+			System.out.println("This program determines the amount of quarters, dimes, nickles, and pennies that will be returned to a");
+			System.out.println("customer after a customer makes a purchase.");
+			System.out.println();
+			System.out.println("Please enter the change due to the customer (Please note, change should be written with a decimal point e.g., .99 cents) : ");
+			System.out.println();
+			
+			double totalChangeDue = input.nextDouble();
+			
+			System.out.println();
+			
+			int calculatedNumberOfQuarters = calculateNumberOfQuarters(totalChangeDue);
+			int calculatedNumberOfDimes = calculateNumberOfDimes(totalChangeDue, calculatedNumberOfQuarters);
+			int calculatedNumberOfNickels = calculateNumberOfNickels(totalChangeDue, calculatedNumberOfQuarters, calculatedNumberOfDimes);
+			int calculatedNumberOfPennies = calculateNumberOfPennies(totalChangeDue, calculatedNumberOfQuarters, calculatedNumberOfDimes, calculatedNumberOfNickels);
+			
+			System.out.println("Quarters: " + calculatedNumberOfQuarters);
+			System.out.println("Dimes: " + calculatedNumberOfDimes);
+			System.out.println("Nickels: " + calculatedNumberOfNickels);
+			System.out.println("Pennies: " + calculatedNumberOfPennies);
+			
+			input.close();
+			
+		}
+		
+		private static int calculateNumberOfQuarters(double totalChangeDue) {
+			
+			int valueOfQuarter = 25;
+			
+			int calculatedNumberOfQuarters = (int) ((totalChangeDue * 100) / valueOfQuarter);
+			
+			return calculatedNumberOfQuarters;
+			
+		}
+		
+		private static int calculateNumberOfDimes(double totalChangeDue, int calculatedNumberOfQuarters) {
+			
+			int valueOfQuarter = 25;
+			int valueOfDime = 10;
+			
+			int calculatedNumberOfDimes = (int) (((totalChangeDue * 100) - (calculatedNumberOfQuarters * valueOfQuarter)) / valueOfDime);
+			
+			return calculatedNumberOfDimes;
+			
+		}
+
+		private static int calculateNumberOfNickels(double totalChangeDue, int calculatedNumberOfQuarters, int calculatedNumberOfDimes) {
+			
+			int valueOfQuarter = 25;
+			int valueOfDime = 10;
+			int valueOfNickel = 5;
+			
+			int calculatedNumberOfNickels = (int) (((totalChangeDue * 100) - (calculatedNumberOfQuarters * valueOfQuarter) - (calculatedNumberOfDimes * valueOfDime)) / valueOfNickel);
+			
+			return calculatedNumberOfNickels;
+			
+		}
+
+		private static int calculateNumberOfPennies(double totalChangeDue, int calculatedNumberOfQuarters, int calculatedNumberOfDimes, int calculatedNumberOfNickels) {
+			
+			int valueOfQuarter = 25;
+			int valueOfDime = 10;
+			int valueOfNickel = 5;
+			int valueOfPenny = 1;
+			
+			int calculatedNumberOfPennies = (int) (((totalChangeDue * 100) - (calculatedNumberOfQuarters * valueOfQuarter) - (calculatedNumberOfDimes * valueOfDime) - (calculatedNumberOfNickels * valueOfNickel)) / valueOfPenny);
+			
+			return calculatedNumberOfPennies;
+			
+		}
+
+}
+
+
