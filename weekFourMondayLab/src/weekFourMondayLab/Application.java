fa2f8b6c77423f8e4e228aafde02e3b76195c2c2
Kenneth Nicholas <nicholas.kenneth.j@gmail.com>
2019-02-11 20:06:23 -0600

    weekFourMondayLab

---
+++ weekFourMondayLab/src/weekFourMondayLab/Application.java
@@ -0,0 +1,21 @@
+package weekFourMondayLab;
+
+import java.util.Scanner;
+
+public class Application {
+
+	public static void main(String[] args) {
+		
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("Please enter your age: ");
+		
+		String inputValue = input.nextLine();
+		
+	    int age = Integer.parseUnsignedInt(inputValue);
+
+	}
+	
+}
+
+
